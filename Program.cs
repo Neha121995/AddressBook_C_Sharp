257c0eeaebb3c7cc7dc3ab0aaf0bb6bb128eda04
Neha121995 <jagtapneha1212@gmail.com>
2021-07-20 22:43:04 +0530

    [Neha] Added: Function to add new contact and display that contact

--- Program.cs
+++ Program.cs
@@ -1,49 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Address_Book
 {
     class Program
     {
-        String First_Name, Last_Name, Address, City, State;
-        int Zip;
-        long Phone_No;
-        public void Create_Contacts()
+        public void Create_Contacts(Data contact)
         {
             Console.Write("Enter the First Name : ");
-            First_Name = (Console.ReadLine());
+            contact.First_Name = (Console.ReadLine());
 
             Console.Write("Enter the Last Name : ");
-            Last_Name = (Console.ReadLine());
+            contact.Last_Name = (Console.ReadLine());
 
             Console.Write("Enter the Address : ");
-            Address = (Console.ReadLine());
+            contact.Address = (Console.ReadLine());
 
             Console.Write("Enter the City : ");
-            City = (Console.ReadLine());
+            contact.City = (Console.ReadLine());
 
             Console.Write("Enter the State : ");
-            State = (Console.ReadLine());
+            contact.State = (Console.ReadLine());
 
             Console.Write("Enter the Zip : ");
-            Zip = Convert.ToInt32(Console.ReadLine());
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Enter the Phone No : ");
-            Phone_No = Convert.ToInt64(Console.ReadLine());
-        }
+            contact.Phone_No = Convert.ToInt64(Console.ReadLine());
 
-        public void Display_Contacts()
-        {
-            Console.WriteLine("---------------------------------------------------------------------------------------------------");
-            Console.WriteLine("--------------Contacts in Address Books System--------------");
-            Console.WriteLine("First name is :" + First_Name + "\nLast name is :" + Last_Name + "\nAddress is :" + Address + "\nCity is :" + City + "\nState is :" + State + "\nZip code is :" + Zip + "\nPhone no is :" + Phone_No);
+            Console.Write("Enter the Email Id : ");
+            contact.Email = (Console.ReadLine());
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            AdddressBookBuilder addressbook = new AdddressBookBuilder();
             Program addressbookpbj = new Program();
-            addressbookpbj.Create_Contacts();
-            addressbookpbj.Display_Contacts();
+            int choice = 1;
+            while (choice != 3)
+            {
+                Console.WriteLine("Menu : \n 1.Add_Contact \n 2.Display_Contact \n 3.Exit ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Data contact = new Data();
+                        addressbookpbj.Create_Contacts(contact);
+                        addressbook.AddContact(contact);
+                        Console.WriteLine("Contact Added Successfully.");
+                        break;
+                    case 2:
+                        addressbook.Display_Contacts();
+                        break;
+                    case 3:
+                        Console.WriteLine("Program Exited");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input");
+                        break;
+                }
 
+            }
         }
     }
 }
